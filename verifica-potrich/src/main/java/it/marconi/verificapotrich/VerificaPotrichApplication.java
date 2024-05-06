3228c4642cd91b67c2da7fd8a1f3e4aff4e1d110
LANParty2023-24 <elisa.potrich332005@gmail.com>
2024-05-06 08:26:46 +0200

    Creazione progetto

---
+++ verifica-potrich/src/main/java/it/marconi/verificapotrich/VerificaPotrichApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.verificapotrich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificaPotrichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificaPotrichApplication.class, args);
+	}
+
+}
---
+++ verifica-potrich/src/test/java/it/marconi/verificapotrich/VerificaPotrichApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.verificapotrich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificaPotrichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
